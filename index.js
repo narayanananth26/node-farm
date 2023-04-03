44c3489fce25b46a99d3173c1d59e6a30327c0cd
Ananth Narayan <narayanananth26@gmail.com>
2023-04-03 19:59:38 +0530

    build simple API

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const fs = require("fs");
 const http = require("http");
 const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 // SERVER
 const server = http.createServer((req, res) => {
@@ -9,9 +13,13 @@ const server = http.createServer((req, res) => {
 		res.end("This is the OVERVIEW");
 	} else if (pathName === "/product") {
 		res.end("This is the PRODUCT");
+	} else if (pathName === "/api") {
+		res.writeHead(200, { "Content-type": "application/json" });
+		res.end(data);
 	} else {
 		res.writeHead(404, {
 			"Content-type": "text/html",
+			"my-own-header": "hello-world",
 		});
 		res.end("<h1>Page not found!</h1>");
 	}
