e347001a81586d4b16b2b5e2ee2823a78ff84f72
Ananth Narayan <narayanananth26@gmail.com>
2023-04-03 18:45:29 +0530

    read and write files async

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
+// // Synchronous
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-const textOut = `This is what we know about the avacada ${textIn}.\nCreated on ${Date.now}`;
+// const textOut = `This is what we know about the avocado ${textIn}.\nCreated on ${Date.now}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("File written");
 
-fs.writeFileSync('./txt/output.txt', textOut);
+// Asynchronous
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+	fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+		console.log(data2);
 
-console.log('File written');
+		fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+			if (err) return console.log("ERROR");
+			console.log(data3);
+			fs.writeFile(
+				"./txt/final.txt",
+				`${data2}\n${data3}`,
+				"utf-8",
+				(err) => {
+					console.log("Your file has been written");
+				}
+			);
+		});
+	});
+});
+console.log("Will read file");
