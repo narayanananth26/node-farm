22ce730a13e81b88415eb9198fbe8edd39a4ba4b
Ananth Narayan <narayanananth26@gmail.com>
2023-04-04 12:30:57 +0530

    save and use 'replaceTemplate' function as module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const replaceTemplate = require("./modules/replaceTemplate");
 
 const tempOverview = fs.readFileSync(
 	`${__dirname}/templates/template-overview.html`,
@@ -17,26 +18,10 @@ const tempProduct = fs.readFileSync(
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
 
-const replaceTemplate = (temp, product) => {
-	let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-	output = output.replace(/{%IMAGE%}/g, product.image);
-	output = output.replace(/{%PRICE%}/g, product.price);
-	output = output.replace(/{%FROM%}/g, product.from);
-	output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-	output = output.replace(/{%QUANTITY%}/g, product.quantity);
-	output = output.replace(/{%DESCRIPTION%}/g, product.description);
-	output = output.replace(/{%ID%}/g, product.id);
-
-	if (!product.organic)
-		output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-	return output;
-};
-
 // SERVER
 const server = http.createServer((req, res) => {
-	
-	const {query, pathname} = url.parse(req.url, true);
-	
+	const { query, pathname } = url.parse(req.url, true);
+
 	// ROUTING
 
 	// Overview
@@ -52,7 +37,6 @@ const server = http.createServer((req, res) => {
 
 		// Product
 	} else if (pathname === "/product") {
-
 		res.writeHead(200, { "Content-type": "text/html" });
 
 		const product = dataObj[query.id];
